099636130b5336c75c4bdc37223979b594315fae
Anil Varma <varma.rymec@gmail.com>
2025-04-09 14:33:19 +0530

    commit2

--- src/components/Modal/TestModal.jsx
+++ src/components/Modal/TestModal.jsx
@@ -13,15 +13,6 @@ const TestModal = () => {
   //TODO: Add a modal component to the app
   //FIXME: Add a modal component to the app
   // Dummy functions to exceed NOM threshold
-  const func1 = () => console.log('Function 1')
-  const func2 = () => console.log('Function 2')
-  const func3 = () => console.log('Function 3')
-  const func4 = () => console.log('Function 4')
-  const func5 = () => console.log('Function 5')
-  const func6 = () => console.log('Function 6')
-  const func7 = () => console.log('Function 7')
-  const func8 = () => console.log('Function 8')
-  const func9 = () => console.log('Function 9') // 9th function
 
   console.log('methods', handleClose, handleOpen)
 
